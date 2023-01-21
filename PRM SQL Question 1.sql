5ae4a056c0482a5179b0de1b01fcdc4de3908397
TandonPratham <117387692+TandonPratham@users.noreply.github.com>
2023-01-21 13:36:11 +0530

    Add files via upload

---
+++ PRM SQL Question 1.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS countries;
+CREATE TABLE countries(
+country_id VARCHAR(100),
+country_name VARCHAR(100)
+CHECK(country_name IN('Italy','India','China')) ,
+region_id decimal(10,0)
+);
+desc countries;
